9e2127ee72a020b2f4897d01ed6abfa780d53e88
Bosko Stankovic <zekave@yahoo.com>
2018-03-21 09:29:30 +0100

    Tiny change

--- app/src/main/java/com/boxy_apps/boxylullaby/ItemDetailActivity.java
+++ app/src/main/java/com/boxy_apps/boxylullaby/ItemDetailActivity.java
@@ -40,6 +40,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        
 
         // savedInstanceState is non-null when there is fragment state
         // saved from previous configurations of this activity
